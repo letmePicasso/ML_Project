7ec851f78af24175827c78dd286c84f923f4fc48
Ahmad_Analytics <raoahmadnasir0349@gmail.com>
2026-07-27 20:32:57 +0500

    Model train updated

--- src/components/model_train.py
+++ src/components/model_train.py
@@ -54,7 +54,22 @@ class ModelTrainer:
             model_report:dict=evaluate_model(X_train=X_train,Y_train=Y_train,X_test=X_test,Y_test=Y_test
                                              ,models=models)
 
-            
-            return model_report
+            best_model_score=max(sorted(model_report.values()))
+
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model=models[best_model_name]
+            if best_model_score<0.6:
+                raise CustomException("No best model is found")
+            logging.info("Best model is found and trained")
+            save_object(
+                file_path=ModelTrainerConfig.trained_model_file_path,
+                obj=best_model
+            )
+            predicted=best_model.predict(X_test)
+            r2_square=r2_score(Y_test,predicted)
+            return r2_square
         except Exception as e:
             raise CustomException(e,sys)
