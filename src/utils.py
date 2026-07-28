c6783625756f139a82865f53157d4f04bc45b34d
Ahmad_Analytics <raoahmadnasir0349@gmail.com>
2026-07-29 03:45:24 +0500

    Web framework implemented

--- src/utils.py
+++ src/utils.py
@@ -38,4 +38,12 @@ def evaluate_model(X_train,Y_train,X_test,Y_test,models,params):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
             
